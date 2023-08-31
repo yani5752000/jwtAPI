fd9d7d66c4f7160618bd9deb251251a918c9a4b8
Farid Asadpour <84594007+yani5752000@users.noreply.github.com>
2023-08-31 16:01:32 -0400

    model

---
+++ expressServer/models/user_model.js
@@ -0,0 +1,21 @@
+const  Pool = require("pg").Pool;
+const pool = new Pool({
+    host: "localhost";
+    database: "jwt_database",
+    user: "jwt_user",
+    password: "root",
+    port: 5432
+})
+
+const addUser = ({username, email, password}) => {
+    const queryString = "INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING *;";
+    return new Promise((resolve, reject) => {
+        pool.query(queryString, [username, email, password], (error, result) => {
+            if(error) {
+                reject(error);
+            } else {
+                resolve(result.rows);
+            }
+        })
+    })
+}
---
+++ frontend/src/pages/register.js
@@ -0,0 +1,16 @@
+export default function Register() {
+    return (
+    <>
+        <form>
+            <label>Username</label>
+            <input type="text"></input>
+            <label>Email</label>
+            <input type="email"></input>
+            <label>Password</label>
+            <input type="password"></input>
+            <button type="submit">Register</button>
+        </form>
+        Register here
+    </>
+    )
+}
